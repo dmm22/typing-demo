378cea94b0dbd6799bc1fd131db1ceeb98fac4dc
dmm22 <davidmartin71090@gmail.com>
2024-07-08 11:13:49 -0400

    feat: set up React Router and define main routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import TestPage from "./pages/Test/TestPage"
+import TestResultsPage from "./pages/TestResults/TestResultsPage"
+import UserProfilePage from "./pages/UserProfile/UserProfilePage"
+
 export default function App() {
   return (
-    <main>
-      <p>App</p>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TestPage />} />
+        <Route path="/results" element={<TestResultsPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
