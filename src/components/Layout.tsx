64edb5175109a8f647a6254ed289a5dc14c3d8f6
dmm22 <davidmartin71090@gmail.com>
2024-07-12 12:14:38 -0400

    feat: add Navbar component to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom"
 import NewTestButton from "./NewTestButton"
 
 import TestManagerContextProvider from "../context/TestManagerContext"
+import Navbar from "./Navbar"
 
 export default function Layout() {
   return (
     <TestManagerContextProvider>
+      <Navbar />
       <NewTestButton />
       <Outlet />
     </TestManagerContextProvider>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import { FaUser } from "react-icons/fa"
+
+export default function Navbar() {
+  return (
+    <nav className="relative z-10 flex items-center justify-between mb-3">
+      <Link className="text-3xl font-bold text-sky-600" to="/" tabIndex={1}>
+        Logo
+      </Link>
+      <ul className="flex items-center gap-8">
+        <li>
+          <Link to="/profile" tabIndex={3}>
+            <FaUser className="p-1.5 text-4xl text-white rounded-full bg-neutral-600" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
